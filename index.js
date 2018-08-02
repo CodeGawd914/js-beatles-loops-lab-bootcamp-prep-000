262e884a5b5b5d1ed97cc23e7cf3da418c3064c5
travisgdesir31@gmail.com <travisgdesir31@gmail.com>
2018-08-02 21:08:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// add solution here
+function theBeatlesPlay(musicians,instruments){
+  var array =[]
+    for (var i = 0; i < musicians.length ; i++){
+   array.push(`${musicians[i]} plays ${instruments[i]}`)
+  }
+  return array
+}
+function johnLennonFacts(facts){
+  while (0>facts.length){facts.push('!!!')
+}
+return facts
+}
+function iLoveTheBeatles(number){
+  var array = []
+  array.push(`I love the Beatles!`)
+}
