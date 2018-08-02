2be1a3c7eea6cea9a9e6aa3173925283126cad82
travisgdesir31@gmail.com <travisgdesir31@gmail.com>
2018-08-02 21:20:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,8 +8,9 @@ function theBeatlesPlay(musicians,instruments){
 function johnLennonFacts(facts){
   var i = 0
   var array =[]
-  while (i<facts.length){i++
+  while (i<facts.length){
     array.push(`${facts[i]}!!!`)
+    i++
 }
 return array
 }
