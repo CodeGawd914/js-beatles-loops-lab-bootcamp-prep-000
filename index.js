e49537c43af8afb87fd2fc75faa3c0d47ee929fd
travisgdesir31@gmail.com <travisgdesir31@gmail.com>
2018-08-02 21:41:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,9 +16,7 @@ return array
 }
 function iLoveTheBeatles(number){
   var array = []
-  do {array.push(`I love the Beatles!`)number++
-}
+  do {array.push(`I love the Beatles!`); number++}
 while (number < 15)
-
 return array
 }
